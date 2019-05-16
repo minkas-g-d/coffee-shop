88e208a0b629564206553aee8a650cc55bc60924
minkas-g-d <mina.dodunekova@gmail.com>
2019-05-16 16:16:25 +0300

    indentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,48 +10,48 @@ import Products from "./../components/Home/Products"
 import Contact from "./../components/Home/Contact"
 
 const IndexPage = ({ data }) => (
-	<Layout>
-		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-		<BackgroundSection
-		img={data.img.childImageSharp.fluid}
-		title="regular joe's"
-		styleClass="default-background"
-		/>
-		<Info />
-		<Menu items={data.menu} />
-		<Products />
-		<Contact />
-	</Layout>
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+      styleClass="default-background"
+    />
+    <Info />
+    <Menu items={data.menu} />
+    <Products />
+    <Contact />
+  </Layout>
 )
 
 export const query = graphql`
-	{
-		img: file(relativePath: { eq: "default-background.jpeg" }) {
-		childImageSharp {
-			fluid {
-			...GatsbyImageSharpFluid_tracedSVG
-			}
-		}
-		}
-		menu: allContentfulCoffeeItem {
-		edges {
-			node {
-			id
-			title
-			description {
-				description
-			}
-			price
-			category
-			image {
-				fixed(width: 50, height: 50) {
-				...GatsbyContentfulFixed_tracedSVG
-				}
-			}
-			}
-		}
-		}
-	}
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
 `
 
 export default IndexPage
