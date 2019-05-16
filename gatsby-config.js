fef4b9b1be607a2f6957de42ef394867ab72d2ec
minkas-g-d <mina.dodunekova@gmail.com>
2019-05-16 11:54:54 +0300

    add contact component and implement snipcart integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,14 @@ module.exports = {
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
-    }
+    },
+		{
+			resolve: 'gatsby-plugin-snipcart',
+			options: {
+        apiKey: process.env.SNIPCART_API_KEY,
+        autopop: true
+			}
+		}
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
