07484a1fa2b9bb68b350d42088992b35088284e7
minkas-g-d <mina.dodunekova@gmail.com>
2019-05-16 15:49:59 +0300

    Fix indentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,55 +3,55 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import BackgroundSection from "./../components/globals/BackgroundSection";
-import Info from "./../components/Home/Info";
-import Menu from "./../components/Home/Menu";
-import Products from "./../components/Home/Products";
-import Contact from "./../components/Home/Contact";
+import BackgroundSection from "./../components/globals/BackgroundSection"
+import Info from "./../components/Home/Info"
+import Menu from "./../components/Home/Menu"
+import Products from "./../components/Home/Products"
+import Contact from "./../components/Home/Contact"
 
-const IndexPage = ( { data } ) => (
-  <Layout>
-	<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-	<BackgroundSection
-	  img={ data.img.childImageSharp.fluid }
-	  title="regular joe's"
-	  styleClass="default-background"
-	/>
-	<Info />
-	<Menu items={ data.menu } />
-	<Products />
-	<Contact />
-  </Layout>
-);
+const IndexPage = ({ data }) => (
+	<Layout>
+		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+		<BackgroundSection
+		img={data.img.childImageSharp.fluid}
+		title="regular joe's"
+		styleClass="default-background"
+		/>
+		<Info />
+		<Menu items={data.menu} />
+		<Products />
+		<Contact />
+	</Layout>
+)
 
 export const query = graphql`
-  {
-	img: file(relativePath: { eq: "default-background.jpeg" }) {
-	  childImageSharp {
-		fluid {
-		  ...GatsbyImageSharpFluid_tracedSVG
-		}
-	  }
-	}
-	menu: allContentfulCoffeeItem {
-	  edges {
-		node {
-		  id
-		  title
-		  description {
-			description
-		  }
-		  price
-		  category
-		  image {
-			fixed(width: 50, height: 50) {
-			  ...GatsbyContentfulFixed_tracedSVG
+	{
+		img: file(relativePath: { eq: "default-background.jpeg" }) {
+		childImageSharp {
+			fluid {
+			...GatsbyImageSharpFluid_tracedSVG
 			}
-		  }
 		}
-	  }
+		}
+		menu: allContentfulCoffeeItem {
+		edges {
+			node {
+			id
+			title
+			description {
+				description
+			}
+			price
+			category
+			image {
+				fixed(width: 50, height: 50) {
+				...GatsbyContentfulFixed_tracedSVG
+				}
+			}
+			}
+		}
+		}
 	}
-  }
-`;
+`
 
 export default IndexPage
