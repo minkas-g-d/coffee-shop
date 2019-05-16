631493614ee530873b853665b45edfd5e0a42bce
minkas-g-d <mina.dodunekova@gmail.com>
2019-05-16 12:22:08 +0300

    test why build does not want to pass

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,47 +11,47 @@ import Contact from "./../components/Home/Contact";
 
 const IndexPage = ( { data } ) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <BackgroundSection
-      img={ data.img.childImageSharp.fluid }
-      title="regular joe's"
-      styleClass="default-background"
-    />
-    <Info />
-    <Menu items={ data.menu } />
-    <Products />
-    <Contact />
+	<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+	<BackgroundSection
+	  img={ data.img.childImageSharp.fluid }
+	  title="regular joe's"
+	  styleClass="default-background"
+	/>
+	<Info />
+	<Menu items={ data.menu } />
+	<Products />
+	<Contact />
   </Layout>
 );
 
 export const query = graphql`
-{
-  img: file(relativePath: {eq: "default-background.jpeg"}) {
-    childImageSharp {
-      fluid {
-        ...GatsbyImageSharpFluid_tracedSVG
-      }
-    }
+  {
+	img: file(relativePath: { eq: "default-background.jpeg" }) {
+	  childImageSharp {
+		fluid {
+		  ...GatsbyImageSharpFluid_tracedSVG
+		}
+	  }
+	}
+	menu: allContentfulCoffeeItem {
+	  edges {
+		node {
+		  id
+		  title
+		  description {
+			description
+		  }
+		  price
+		  category
+		  image {
+			fixed(width: 50, height: 50) {
+			  ...GatsbyContentfulFixed_tracedSVG
+			}
+		  }
+		}
+	  }
+	}
   }
-  menu:allContentfulCoffeeItem{
-    edges {
-      node {
-        id
-        title
-        description {
-          description
-        }
-        price
-        category
-        image {
-          fixed(width:50,height:50) {
-            ...GatsbyContentfulFixed_tracedSVG
-          }
-        }
-      }
-    }
-  }
-}
 `;
 
 export default IndexPage
